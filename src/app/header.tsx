925b19418272a5cde6f10dab97878d14c83479eb
Bhanuka Pannipitiya <bhanuka.pannipitiya@innovative-e.com>
2024-07-27 20:57:23 +0530

    Added the middleware to handle api calls securely using convex

---
+++ src/app/header.tsx
@@ -0,0 +1,12 @@
+import { UserButton } from "@clerk/nextjs";
+
+export function Header() {
+    return (
+      <div className="border-b py-4 bg-gray-50">
+        <div className="items-center container mx-auto justify-between flex">
+            MySpace
+            <UserButton />
+        </div>
+      </div>
+    );
+  }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+"use client";
 import { Button } from "@/components/ui/button";
 import {
   SignIn,
@@ -7,9 +8,18 @@ import {
   SignedOut,
   useSession,
 } from "@clerk/nextjs";
+import { useMutation, useQuery } from "convex/react";
 import Image from "next/image";
+import { api } from "../../convex/_generated/api";
 
 export default function Home() {
+  const files = useQuery(api.files.getFiles);
+  const createFile = useMutation(api.files.createFile); 
+
+  //onClick handler that calls the createFile mutation with a static name
+  const handleCreateFileClick = () => {
+    createFile({ name: "NewFile" });
+  };
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <SignedIn>
@@ -17,10 +27,16 @@ export default function Home() {
         <SignOutButton></SignOutButton>
       </SignedIn>
       <SignedOut>
-        <SignInButton mode="modal">
-        <Button>Sign In</Button>
+        <SignInButton>
+          <Button>Sign In</Button>
         </SignInButton>
       </SignedOut>
+
+      {files?.map((file) => {
+        return <div key={file._id}>{file.name}</div>;
+      })}
+      <Button onClick={handleCreateFileClick}>Press me to create a file</Button>
+
     </main>
   );
 }
